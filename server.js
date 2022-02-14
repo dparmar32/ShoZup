47b78e8a921a1994455a2c13cac202acc7269653
Jonathan Sewell <jsewellro@gmail.com>
2022-02-14 16:50:42 -0400

    first controller commit

--- server.js
+++ server.js
@@ -5,12 +5,8 @@ const Sequelize = require("sequelize");
 const exphbs = require('express-handlebars')
 const hbs = exphbs.create({})
 const path = require('path');
-
-
 const bcrypt = require('bcrypt') //to encrypt password, adding more security to our users
 // const Joi = require('joi');
-
-
 
 
 // Import the connection object
@@ -28,7 +24,8 @@ app.use(express.urlencoded({extended: true}));
 //turn on routes
 // app.use(routes);
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(require('./controllers/dish-routes.js'));
+app.use(require('./controllers/api/userRoutes.js'));
+app.use(require('./controllers/api/projectRoutes.js'));
 
 // Connect to the database before starting the Express.js server
 // Starts the server to begin listening
