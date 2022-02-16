4fa1531acc246b611b8674756e558861b49dbc99
Drashtee <drashteeparmar@gmail.com>
2022-02-15 21:32:29 -0500

    server working

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,10 +1,10 @@
 const router = require('express').Router();
-const userRoutes = require('./userRoutes');
-const projectRoutes = require('./projectRoutes');
-const categoryRoutes = require('./categoryRoutes');
+const userRoutes = require('./user-routes');
+// const projectRoutes = require('./projectRoutes');
+// const categoryRoutes = require('./categoryRoutes');
 
 router.use('/users', userRoutes);
-router.use('/projects', projectRoutes);
-router.use('/category',categoryRoutes);
+// router.use('/projects', projectRoutes);
+// router.use('/category',categoryRoutes);
 
 module.exports = router;
