8be4ad735a5ba5e08a0c09da958f381746e167c1
Maria Cardona <mechas8703@hotmail.com>
2022-02-18 00:40:24 -0500

    routing logout to login page

--- public/js/logout.js
+++ public/js/logout.js
@@ -4,11 +4,20 @@ const logout = async () => {
     headers: { 'Content-Type': 'application/json' },
   });
 
-  if (response.ok) {
-    document.location.replace('/');
-  } else {
-    alert(response.statusText);
-  }
-};
+ 
+    if (response.ok) {
+      // If successfully logged out, redirect to the login page
+      document.location.replace('/login');
+    } else {
+      alert(response.statusText);
+    }
+  };
+
+//   if (response.ok) {
+//     document.location.replace('/');
+//   } else {
+//     alert(response.statusText);
+//   }
+// };
 
 document.querySelector('#logout').addEventListener('click', logout);
